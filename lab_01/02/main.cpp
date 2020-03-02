8483c7b3794ac8b6bbf5177986e4620dfafe7368
arina-kaa <Arina.kaa@inbox.ru>
2020-03-02 23:47:46 +0300

    Добавила 2 задание 1 лабораторной работы

--- lab_01/02/main.cpp
+++ lab_01/02/main.cpp
@@ -1,15 +1,15 @@
-// CPP program to print all permutations using 
-// Johnson and Trotter algorithm. 
 #include <iostream>
 #include <boost/timer.hpp>
 
 using namespace std;
 
-bool LEFT_TO_RIGHT = true;
-bool RIGHT_TO_LEFT = false;
+enum Direction
+{
+    LEFT_TO_RIGHT,
+    RIGHT_TO_LEFT
+};
 
-// Utility functions for finding the 
-// position of largest mobile integer in a[]. 
+// utility functions for finding the position of largest mobile integer in a[]
 int searchArr(int a[], int n, int mobile)
 {
     for (int i = 0; i < n; i++)
@@ -17,31 +17,26 @@ int searchArr(int a[], int n, int mobile)
             return i + 1;
 }
 
-// To carry out step 1 of the algorithm i.e. 
-// to find the largest mobile integer. 
-int getMobile(int a[], bool dir[], int n)
+// to carry out step 1 of the algorithm to find the largest mobile integer.
+int getMobile(int a[], Direction dir[], int n)
 {
     int mobile_prev = 0, mobile = 0;
     for (int i = 0; i < n; i++)
     {
         // direction 0 represents RIGHT TO LEFT. 
-        if (dir[a[i] - 1] == RIGHT_TO_LEFT && i != 0)
+        if (dir[a[i] - 1] == Direction::RIGHT_TO_LEFT && i != 0 
+            && a[i] > a[i - 1] && a[i] > mobile_prev)
         {
-            if (a[i] > a[i - 1] && a[i] > mobile_prev)
-            {
-                mobile = a[i];
-                mobile_prev = mobile;
-            }
+            mobile = a[i];
+            mobile_prev = mobile;
         }
 
         // direction 1 represents LEFT TO RIGHT. 
-        if (dir[a[i] - 1] == LEFT_TO_RIGHT && i != n - 1)
+        if (dir[a[i] - 1] == Direction::LEFT_TO_RIGHT && i != n - 1 
+            && a[i] > a[i + 1] && a[i] > mobile_prev)
         {
-            if (a[i] > a[i + 1] && a[i] > mobile_prev)
-            {
-                mobile = a[i];
-                mobile_prev = mobile;
-            }
+            mobile = a[i];
+            mobile_prev = mobile;
         }
     }
 
@@ -51,41 +46,35 @@ int getMobile(int a[], bool dir[], int n)
         return mobile;
 }
 
-// Prints a single permutation 
-void printOnePerm(int a[], bool dir[], int n)
+// getting a single permutation 
+void getNextPermutation(int a[], Direction dir[], int n)
 {
     int mobile = getMobile(a, dir, n);
     int pos = searchArr(a, n, mobile);
 
-    // swapping the elements according to the 
-    // direction i.e. dir[]. 
-    if (dir[a[pos - 1] - 1] == RIGHT_TO_LEFT)
+    // swapping the elements according to the direction 
+    if (dir[a[pos - 1] - 1] == Direction::RIGHT_TO_LEFT)
         swap(a[pos - 1], a[pos - 2]);
-
-    else if (dir[a[pos - 1] - 1] == LEFT_TO_RIGHT)
+    else if (dir[a[pos - 1] - 1] == Direction::LEFT_TO_RIGHT)
         swap(a[pos], a[pos - 1]);
 
-    // changing the directions for elements 
-    // greater than largest mobile integer. 
+    // changing the directions for elements greater than largest mobile integer
     for (int i = 0; i < n; i++)
     {
         if (a[i] > mobile)
         {
-            if (dir[a[i] - 1] == LEFT_TO_RIGHT)
-                dir[a[i] - 1] = RIGHT_TO_LEFT;
-            else if (dir[a[i] - 1] == RIGHT_TO_LEFT)
-                dir[a[i] - 1] = LEFT_TO_RIGHT;
+            if (dir[a[i] - 1] == Direction::LEFT_TO_RIGHT)
+                dir[a[i] - 1] = Direction::RIGHT_TO_LEFT;
+            else if (dir[a[i] - 1] == Direction::RIGHT_TO_LEFT)
+                dir[a[i] - 1] = Direction::LEFT_TO_RIGHT;
         }
     }
 
-    for (int i = 0; i < n; i++)
-        cout << a[i];
-    cout << endl;
+    /*for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;*/
 }
 
-// To end the algorithm for efficiency it ends 
-// at the factorial of n because number of 
-// permutations possible is just n!. 
 int fact(int n)
 {
     int res = 1;
@@ -94,39 +83,33 @@ int fact(int n)
     return res;
 }
 
-// Driver code 
 int main()
 {
-    const int n = 5;
-    // To store current permutation 
+    const int n = 13;
     int a[n];
-
-    // To store current directions 
-    bool dir[n];
+    Direction dir[n];
 
     boost::timer t;
     t.restart();
 
-    // storing the elements from 1 to n and 
-    // printing first permutation. 
+    // set the elements from 1 to n
     for (int i = 0; i < n; i++)
     {
         a[i] = i + 1;
-        cout << a[i];
+        //cout << a[i] << " ";
     }
-    cout << endl;
+    //cout << endl;
 
-    // initially all directions are set 
-    // to RIGHT TO LEFT i.e. 0. 
+    // initially all directions are set to RIGHT TO LEFT
     for (int i = 0; i < n; i++)
-        dir[i] = RIGHT_TO_LEFT;
+        dir[i] = Direction::RIGHT_TO_LEFT;
 
-    // for generating permutations in the order. 
+    // for generating permutations in the order
     for (int i = 1; i < fact(n); i++)
-        printOnePerm(a, dir, n);
+        getNextPermutation(a, dir, n);
 
     double duration = t.elapsed();
-    std::cout << duration << std::endl;
+    cout << duration << endl;
 
     return 0;
 }
